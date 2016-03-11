e4e004b7a51c9ecef05688422d91b4a75d8a2334
Don Neill <neilljd@gmail.com>
2016-03-11 13:48:30 -0800

    Added comments for PEP0008 compliance and clarity

--- SEDMr/Cube.py
+++ SEDMr/Cube.py
@@ -162,26 +162,34 @@ def extraction_to_cube(exts, outname="G.npy", fidwave=None):
 
     meta = {"fiducial_wavelength": fid_wave}
 
+    # start with our x,y pixel positions
     Xs = [None] * len(exts)
     Ys = [None] * len(exts)
 
+    # these are the hex axial coords
     for ext in exts:
         ext.Q_ix = None
         ext.R_ix = None
     
+    # counters for which wavelength solution is used:
+    # mdn = median, lam = lambda, tot = total used
     n_mdn = 0
     n_lam = 0
     n_tot = 0
 
     for ix, ext in enumerate(exts):
         # The X/Y location of a lenslet is based on its
-        # trace Y position and where the Halpha wavelength
+        # trace Y position and where the fiducial wavelength
         # is expected in the X direction.
+
+        # skip bad solutions
         if not ext.ok: continue
 
+        # initialize
         Xs[ix] = -999
         Ys[ix] = -999
 
+        # lamcoeff has precidence
         if ext.lamcoeff is not None:
             coeff = ext.lamcoeff
             n_lam += 1
@@ -193,26 +201,35 @@ def extraction_to_cube(exts, outname="G.npy", fidwave=None):
 
         n_tot += 1
 
+        # get pixel and wavelength vectors
         ixs = np.arange(*ext.xrange)
         LL = chebval(ixs, coeff)
         
-        ix_ha = np.nanargmin(np.abs(LL-fid_wave))
+        # fiducial wavelength index
+        ix_fid = np.nanargmin(np.abs(LL-fid_wave))
 
-        Xs[ix] = ixs[ix_ha]
+        # fill in the x,y pix positions
+        Xs[ix] = ixs[ix_fid]
         Ys[ix] = np.nanmean(ext.yrange)
         ext.X_pix = Xs[ix]
         ext.Y_pix = Ys[ix]
+    # End loop over all extractions
 
-
+    # make arrays
     Xs = np.array(Xs, dtype=np.float)
     Ys = np.array(Ys, dtype=np.float)
+    # replace None's with -999
     Xs[Xs != Xs] = -999
     Ys[Ys != Ys] = -999
-    dat = np.array([Xs,Ys],dtype=np.float).T
 
+    # Make a K-Tree
+    dat = np.array([Xs,Ys],dtype=np.float).T
     tree = KDTree(dat)
 
+    # Get the index of the spaxel closest to the center of the CCD
     ignore, Center = tree.query([1024,1024], 1)
+
+    # Set the coords for the center to 0,0 (origin)
     exts[Center].Q_ix = 0
     exts[Center].R_ix = 0
 
@@ -226,9 +243,14 @@ def extraction_to_cube(exts, outname="G.npy", fidwave=None):
             the relative offset based on the rotation matrix defined
             earlier in the file
         """
+        # NOTE: self refers to the extraction with index in
+        # the called parameter (to_populate).
+
         # Query 7 for the object + six surrounding members
+        # Generate pixel distances relative to self and indices
         v = np.array([Xs[to_populate], Ys[to_populate]])
         Dists, Ixs = tree.query(v, 7)
+        # Transformation matrix
         Tfm = P2H * ROT / np.median(Dists) * 2
 
         # Trim self reference
@@ -241,61 +263,84 @@ def extraction_to_cube(exts, outname="G.npy", fidwave=None):
         Dists = Dists[ok]
         Ixs = Ixs[ok]
 
+        # Get the Q,R of the extraction we are populating
         q_this = exts[to_populate].Q_ix
         r_this = exts[to_populate].R_ix
 
-        # Loop over the current nearest spaxels
+        # Loop over the current nearest extractions
         for nix in Ixs:
-            # Search around the current hex via a recrusive call
+            # Search around current hex via a recrusive call
             # to populate_hex
 
+            # Current extraction
             nv = np.array([Xs[nix], Ys[nix]])
+            # Offset between self and current
             D = nv-v
+            # Get offset in Q, R frame
             dp = np.dot(Tfm, D)
+            # Integer hex positions so we round
             rnd = np.round(dp)
 
+            # Coincident with center spaxel, so skip
             if rnd[0] == 0 and rnd[1] == 0:
                 continue
 
+            # Offset larger than a single hex, so skip
             if np.abs(rnd[0]) > 1 or np.abs(rnd[1]) > 1:
                 continue
 
+            # The first time populating and then drill further
             if exts[nix].Q_ix is None:
                 exts[nix].Q_ix = q_this + rnd[0]
                 exts[nix].R_ix = r_this + rnd[1]
                 populate_hex(nix)
+            # We've populated this one before
             else:
+                # Check if we are referring to the
+                # same hex position.  If not, we have
+                # a collision.
                 if (exts[nix].Q_ix != q_this + rnd[0]) or \
                     (exts[nix].R_ix != r_this + rnd[1]):
                     print "collision: ",
                     print exts[nix].Q_ix, q_this + rnd[0], " ",
                     print exts[nix].R_ix, r_this + rnd[1]
+                    # Update with this one, but don't drill more
                     exts[nix].Q_ix = q_this + rnd[0]
                     exts[nix].R_ix = r_this + rnd[1]
-
+        # end loop over current nearest extractions
+    # end def populate_hex
 
     populate_hex(Center)
 
     # Now convert Q/R to even-Q X/Y
     #
 
+    # Get arrays of hex positions
     Qs = np.array([ext.Q_ix for ext in exts], dtype=np.float)
     Rs = np.array([ext.R_ix for ext in exts], dtype=np.float)
 
+    # convert to pixel X,Y
     Xs = np.sqrt(3) * (Qs + Rs/2.0)
     Ys = 3/2 * Rs
 
-    # Note 0.633 is plate scale measured on 22 May 2014.
-    
     #t= np.radians(165.0+45)
+    # Hex angle relative to positive Y pixel axis
     t= np.radians(180+22)
+    # Rotation matrix
     Rot = np.array([[np.cos(t), -np.sin(t)],
                     [np.sin(t),  np.cos(t)]])
+
+    
+    # Loop over extractions and project onto X,Y
     for ix, ext in enumerate(exts):
+        # Project into X,Y frame
         p = np.dot(Rot , np.array([Xs[ix], Ys[ix]]))
+        # Convert to RA, Dec
+        # Note 0.633 is plate scale measured on 22 May 2014.
         ext.X_as = p[0] * -0.633
         ext.Y_as = p[1] * 0.633
 
+    # Write out the cube
     np.save(outname, [exts, meta])
     print "Wrote %s.npy" % outname
 
