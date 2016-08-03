6055b74d6e52a42e0013506cbab7c43e2e7e9bb8
Don Neill <neilljd@gmail.com>
2016-08-03 10:31:56 -0700

    PEP008 compliance

--- NPK/Standards.py
+++ NPK/Standards.py
@@ -1,29 +1,30 @@
-'''Loads standard spectra from Oke 1990 (Aj, 99, 1621).
+"""Loads standard spectra from Oke 1990 (Aj, 99, 1621).
 
-See: 
+See:
     ftp://ftp.eso.org/pub/stecf/standards/okestan/aaareadme.oke
 
 Defines:
     Standards[standard_name]: [Wavelength, flux, flux, bin]
     units: 'Wavelength [A], Flux [erg/s/cm/cm/A 10**16], flux [mJy], bin [A]'
-'''
+"""
+
 import os
 import numpy as np
 
 
-
 units = 'Wavelength [A], Flux [erg/s/cm/cm/A 10**16], flux [mJy], bin [A]'
 
-dir = os.path.join(os.path.dirname(__file__), 'standard_stars')
+sdir = os.path.join(os.path.dirname(__file__), 'standard_stars')
 
-files = os.listdir(dir)
+files = os.listdir(sdir)
 
 Standards = {}
 
-for file in files:
-    if file[0] != 'f': continue
+for fil in files:
+    if fil[0] != 'f':
+        continue
 
-    std_name = file[1:-4]
-    dat = np.loadtxt(os.path.join(dir, file))
+    std_name = fil[1:-4]
+    dat = np.loadtxt(os.path.join(sdir, fil))
     Standards[std_name] = dat
 
