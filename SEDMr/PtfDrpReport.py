48fbbe668713ea35f173f431caaf72960dd29150
Don Neill <neilljd@gmail.com>
2016-10-18 08:42:40 -0700

    typo correction

--- SEDMr/PtfDrpReport.py
+++ SEDMr/PtfDrpReport.py
@@ -98,4 +98,4 @@ if __name__ == '__main__':
         cmd = 'cat report_ptf.txt | mail -s "SEDM DRP Report for %s" iptftransient@lists.astro.caltech.edu'%current_dir
         subprocess.call(cmd, shell=True)
     else:
-        print "No PTF objets to report. \n"
+        print "No PTF objects to report. \n"
