0d28c810bfc8ed6308c564fd748c2aaf2c4f3b54
Don Neill <neilljd@gmail.com>
2016-10-20 09:13:22 -0700

    PEP8 compliance

--- SEDMrph/sedmspeccopy.py
+++ SEDMrph/sedmspeccopy.py
@@ -11,16 +11,21 @@ import argparse
 import subprocess
 import re
 
+
 def find_line_match(lines, matchDict):
-    '''
-    Returns the line number of first line matching the position-word pairs provided within matchDict, if present within lines.
+    """
+    Returns the line number of first line matching the position-word pairs
+    provided within matchDict, if present within lines.
     If no match is found, -1 is returned.
-    '''
+    """
     lineNumber = -1
 
     for index in range(0, len(lines)):
         for positionKey in matchDict:
-            if(len(lines[index].split()) == 0 or len(lines[index].split()) <= positionKey or lines[index].split()[positionKey] != matchDict[positionKey]):
+            if(len(lines[index].split()) == 0 or
+                    len(lines[index].split()) <= positionKey or
+                    lines[index].split()[positionKey] !=
+                    matchDict[positionKey]):
                 lineNumber = -1
                 break
             else:
@@ -29,27 +34,30 @@ def find_line_match(lines, matchDict):
 
     return lineNumber
 
+
 def parse_and_fill(spec, snidoutput):
-    '''
-    Receive the SEDM file and the output from snid. It parses the output from snid, fills a dictionary with the results
-    and fills some of the comments in the original file, so that these could be used in the marshal.
-    '''
+    """
+    Receive the SEDM file and the output from snid. It parses the output
+    from snid, fills a dictionary with the results and fills some of the
+    comments in the original file, so that these could be used in the marshal.
+    """
     
-    pars = {"zmed":-1, "zmederr":-1, "agem": -1, "agemerr": -1, "Ia":0, "Ib":0, "Ic":0, "II":0, "NotSN":0, "rlap" : 0, "bestMatchType": "", "bestMatchSubtype" : "", "bestMatchRedshift" : 0}
+    pars = {"zmed": -1, "zmederr": -1, "agem": -1, "agemerr": -1,
+            "Ia": 0, "Ib": 0, "Ic": 0, "II": 0, "NotSN": 0, "rlap": 0,
+            "bestMatchType": "", "bestMatchSubtype": "", "bestMatchRedshift": 0}
     
     with open(snidoutput, "r") as snid:
         
         lines = snid.readlines()
 
-        zmedLine = find_line_match(lines, {0 : 'zmed'})
-        agemLine = find_line_match(lines, {0 : 'agem'})
-        typeIaLine = find_line_match(lines, {0 : 'Ia'})
-        typeIbLine = find_line_match(lines, {0 : 'Ib'})
-        typeIcLine = find_line_match(lines, {0 : 'Ic'})
-        typeIILine = find_line_match(lines, {0 : 'II'})
-        typeNotSNLine = find_line_match(lines, {0 : 'NotSN'})
-        bestMatchLine = find_line_match(lines, {0 : '1'})
-
+        zmedLine = find_line_match(lines, {0: 'zmed'})
+        agemLine = find_line_match(lines, {0: 'agem'})
+        typeIaLine = find_line_match(lines, {0: 'Ia'})
+        typeIbLine = find_line_match(lines, {0: 'Ib'})
+        typeIcLine = find_line_match(lines, {0: 'Ic'})
+        typeIILine = find_line_match(lines, {0: 'II'})
+        typeNotSNLine = find_line_match(lines, {0: 'NotSN'})
+        bestMatchLine = find_line_match(lines, {0: '1'})
 
         pars["zmed"] = float(lines[zmedLine].split()[1])
         pars["zmederr"] = float(lines[zmedLine].split()[2])
@@ -72,47 +80,63 @@ def parse_and_fill(spec, snidoutput):
         
         commentLinesCount = 0
         for line in specLines:
-            if(line.split()[0] == '#'):
+            if line.split()[0] == '#':
                 commentLinesCount += 1
             else:
                 break
 
-        specLines.insert(commentLinesCount, "# SNIDFRAC_NOTSN: " + str(pars["NotSN"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDFRAC_II: " + str(pars["II"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDFRAC_IC: " + str(pars["Ic"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDFRAC_IB: " + str(pars["Ib"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDFRAC_IA: " + str(pars["Ia"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDAGEMERR: " + str(pars["agemerr"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDAGEM: " + str(pars["agem"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDZMEDERR: " + str(pars["zmederr"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDZMED: " + str(pars["zmed"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDMATCHREDSHIFT: " + str(pars["bestMatchRedshift"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDMATCHRLAP: " + str(pars["rlap"]) + "\n")
-        specLines.insert(commentLinesCount, "# SNIDMATCHSUBTYPE: " + pars["bestMatchSubtype"] + "\n")
-        specLines.insert(commentLinesCount, "# SNIDMATCHTYPE: " + pars["bestMatchType"] + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDFRAC_NOTSN: " + str(pars["NotSN"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDFRAC_II: " + str(pars["II"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDFRAC_IC: " + str(pars["Ic"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDFRAC_IB: " + str(pars["Ib"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDFRAC_IA: " + str(pars["Ia"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDAGEMERR: " + str(pars["agemerr"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDAGEM: " + str(pars["agem"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDZMEDERR: " + str(pars["zmederr"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDZMED: " + str(pars["zmed"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDMATCHREDSHIFT: " +
+                         str(pars["bestMatchRedshift"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDMATCHRLAP: " + str(pars["rlap"]) + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDMATCHSUBTYPE: " +
+                         pars["bestMatchSubtype"] + "\n")
+        specLines.insert(commentLinesCount,
+                         "# SNIDMATCHTYPE: " + pars["bestMatchType"] + "\n")
 
     with open(spec, "w") as specOut:
         specOut.write("".join(specLines))
 
+
 def run_snid(specdir, overwrite=False):
-    '''
+    """
     Runs snid in batch mode on all the txt files found in the given directory.
     If a given file was already classified, it skips it, unless overwrite is requested.
-    '''
+    """
     
     for f in glob.glob(os.path.join(specdir, "*.txt")):
 
-        #retrieve the quality of the spectra.
+        # retrieve the quality of the spectra.
         with open(f, "r") as sf:
             a = sf.readlines()
             
             clas = [ai for ai in a if "TYPE" in ai]
             
-            #If the file has been classified, move to the next
+            # If the file has been classified, move to the next
             if len(clas) > 0 and not overwrite:
                 continue
             
-        #Else, we run the classification with snid
+        # Else, we run the classification with snid
         cmd = "snid wmin=4500 wmax=9500 skyclip=1 medlen=20 aband=1 rlapmin=4 plot=0 %s"%f                
         print cmd
         try:
@@ -127,7 +151,7 @@ def run_snid(specdir, overwrite=False):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=
-        '''
+        """
 
         Copies the output ascii spectra to the iPTF marshal
 
@@ -136,8 +160,8 @@ if __name__ == '__main__':
 
         Specify input directory with -d, or --specdir parameters.
         If none specified, use current date directory in /scr2/sedmdrp/redux/
-            
-        ''', formatter_class=argparse.RawTextHelpFormatter)
+
+        """, formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('-d', '--specdir', type=str, dest="specdir",
                         help='Directory with output PTF*_SEDM.txt files.',
@@ -153,9 +177,9 @@ if __name__ == '__main__':
         timestamp = timestamp.split("T")[0].replace("-", "")
         specdir = os.path.join("/scr2/sedmdrp/redux/", timestamp)
     else:
-	specdir = os.path.abspath(specdir)
-	timestamp = os.path.basename(specdir)
-    os.chdir(specdir)
+        specdir = os.path.abspath(specdir)
+        timestamp = os.path.basename(specdir)
+        os.chdir(specdir)
     print os.getcwd()
     sedmfiles = glob.glob("PTF*.txt")
     print "Copying", sedmfiles
@@ -183,7 +207,7 @@ if __name__ == '__main__':
             if not versions.has_key(newname ):
                 versions[newname] = 1
             else:
-                versions[newname] = versions[newname]+1
+                versions[newname] += 1
                 version = versions[newname]
                 
             newname += "_%s_P60_v%d.ascii" % (timestamp, version)
