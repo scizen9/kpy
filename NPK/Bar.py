f5f4d64bcc969ce41c8558e799b6263af3c7a316
Don Neill <neilljd@gmail.com>
2017-03-04 10:34:49 -0800

    Make more compatible with python3, make sure all medians are nanmedians

--- NPK/Bar.py
+++ NPK/Bar.py
@@ -8,15 +8,15 @@ import sys
 
 
 def setup(toolbar_width=40):
-    ''' Initialize console with toolbar_width spaces between [ ]
+    """ Initialize console with toolbar_width spaces between [ ]
 
     Args:
         toolbar_width: Number of spaces between the brackets
 
     Returns:
-        toolbar_width [int]'''
+        toolbar_width [int]"""
 
-    #toolbar_width = 40
+    # toolbar_width = 40
     global n_bar, n_done, upchar
 
     n_bar = toolbar_width
@@ -31,39 +31,37 @@ def setup(toolbar_width=40):
 
 
 def update(char='-'):
-    '''Prints char to indicate an update on the toolbar
+    """Prints char to indicate an update on the toolbar
 
     Args:
-        char: The character to print'''
+        char: The character to print"""
 
     global n_bar, n_done, upchar
 
     upchar = char
 
-    if (n_done < n_bar):
-    	sys.stdout.write(char)
-    	sys.stdout.flush()
-	n_done += 1
+    if n_done < n_bar:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    n_done += 1
 
 
 def done(mapped=False):
-    '''Carriage return and flush the console'''
+    """Carriage return and flush the console"""
 
     global n_bar, n_done, upchar
 
     if mapped:
-	sys.stdout.write("\r")
-	sys.stdout.write("[%s]" % (upchar * n_bar))
-    	sys.stdout.write("\n")
-	sys.stdout.flush()
+        sys.stdout.write("\r")
+        sys.stdout.write("[%s]" % (upchar * n_bar))
+        sys.stdout.write("\n")
+        sys.stdout.flush()
     else:
-    	while (n_done < n_bar):
-	    sys.stdout.write(upchar)
-	    sys.stdout.flush()
-	    n_done += 1
+        while n_done < n_bar:
+            sys.stdout.write(upchar)
+            sys.stdout.flush()
+            n_done += 1
 
-	sys.stdout.write("]")
-	sys.stdout.write("\n")
-	sys.stdout.flush()
-
- 
+        sys.stdout.write("]")
+        sys.stdout.write("\n")
+        sys.stdout.flush()
