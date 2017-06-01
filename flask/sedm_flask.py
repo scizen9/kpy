5f24f2a23adef21270b0660ddb53bc20ed3cff58
kvyh <karlvyh@hotmail.com>
2017-06-01 09:54:40 -0700

    project_stat graphing

--- flask/sedm_flask.py
+++ flask/sedm_flask.py
@@ -9,11 +9,12 @@ import os
 import datetime
 from astropy.time import Time
 import numpy as np
-from flask import Flask, request, flash, redirect, render_template, url_for
+from flask import Flask, request, flash, redirect, render_template, url_for, make_response
 from SEDMDb.SedmDb import SedmDB
 from SEDMDb.SedmDb_tools import DbTools
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import RequestForm, RedirectForm, FindObjectForm, LoginForm, SubmitObjectForm, SSOForm, is_safe_url
+# from flask.ext.appbuilder.charts.views import DirectByChartView
 import flask
 import flask_login
 from wtforms import Form, HiddenField
@@ -31,6 +32,11 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
+
+groups = db.execute_sql("SELECT id, designator FROM groups;")
+group_dict = {}
+for group in groups:
+    group_dict[group[1]] = group[0]
 
 
 def radec_str2rad(_ra_str, _dec_str):
@@ -81,15 +87,15 @@ def login():
         user_pass = db.get_from_users(['username', 'password', 'id'], {'username': username})
         if not user_pass:
             message = "Incorrect username or password!"
-            return render_template('header.html', current_user=flask_login.current_user) + \
-                   render_template('login.html', form=form, message=message) + \
-                   render_template('footer.html')
+            return (render_template('header.html') +#, current_user=flask_login.current_user) +
+                    render_template('login.html', form=form, message=message) +
+                    render_template('footer.html'))
 
         elif user_pass[0] == -1:
             message = user_pass[1]
-            return render_template('header.html', current_user=flask_login.current_user) + \
-                   render_template('login.html', form=form, message=message) + \
-                   render_template('footer.html')
+            return (render_template('header.html') +#, current_user=flask_login.current_user) +
+                    render_template('login.html', form=form, message=message) +
+                    render_template('footer.html'))
         elif check_password_hash(user_pass[0][1], flask.request.form['password']):
             user = User()
             user.id = user_pass[0][2]
@@ -104,12 +110,12 @@ def login():
             return redirect(flask.url_for('index'))
         else:
             message = "Incorrect username or password!"
-            return render_template('header.html', current_user=flask_login.current_user) + \
-                   render_template('login.html', form=form, message=message) + \
-                   render_template('footer.html')
-    return render_template('header.html', current_user=flask_login.current_user) + \
-                   render_template('login.html', form=form, message=None) + \
-                   render_template('footer.html')
+            return (render_template('header.html') +#, current_user=flask_login.current_user) +
+                    render_template('login.html', form=form, message=message) +
+                    render_template('footer.html'))
+    return (render_template('header.html') +#, current_user=flask_login.current_user) +
+            render_template('login.html', form=form, message=None) +
+            render_template('footer.html'))
 
 
 @app.route('/request', methods=['GET', 'POST'])
@@ -130,9 +136,9 @@ def requests():
                                   'inidate': form1.inidate.data, 'enddate': form1.enddate.data, 'user_id':0, 'program_id': form1.project.data})
             # TODO: set up user_id and program_id=
             message = req[1]
-            return render_template('header.html', current_user=flask_login.current_user) +\
-                   render_template('request.html', form1=form1, form2=form2, message=message) + \
-                   render_template('footer.html')
+            return (render_template('header.html', current_user=flask_login.current_user) +
+                    render_template('request.html', form1=form1, form2=form2, message=message) +
+                    render_template('footer.html'))
 
     if form2.submit_obj.data and form2.validate_on_submit():
         print 'object search'
@@ -170,12 +176,12 @@ def requests():
                 obj_id = None
 
             form1.object_id.data = obj_id  # set the object_id
-            return render_template('header.html', current_user=flask_login.current_user) + \
-                   render_template('request.html', form1=form1, form2=form2, message=message) + \
-                   render_template('footer.html')
-    return render_template('header.html', current_user=flask_login.current_user) + \
-           render_template('request.html', form1=form1, form2=form2, message=False) + \
-           render_template('footer.html')
+            return (render_template('header.html') +#, current_user=flask_login.current_user) +
+                    render_template('request.html', form1=form1, form2=form2, message=message) +
+                    render_template('footer.html'))
+    return (render_template('header.html') +#, current_user=flask_login.current_user) +
+            render_template('request.html', form1=form1, form2=form2, message=False) +
+            render_template('footer.html'))
 # TODO: set up ability to view and cancel requests
 # TODO: allow searching for objects, return link to page with information/observations of it
 # TODO: End of Night report as html
@@ -185,9 +191,9 @@ def requests():
 def schedule():
     # TODO: create a table
     schedule = None  # TODO: decide how schedule will be stored (file/database?)
-    return render_template('header.html', current_user=flask_login.current_user) + \
-           render_template('schedule.html') + \
-           render_template('footer.html')
+    return (render_template('header.html') +#, current_user=flask_login.current_user) +
+            render_template('schedule.html') +
+            render_template('footer.html'))
 
 
 @app.route('/objects', methods=['GET', 'POST'])
@@ -221,10 +227,10 @@ def objects():
                 return redirect(url_for('show_objects', ident=found[0][0]))
     # TODO: make an objects "homepage" to have as the base render
     form2.radius.data = 4
-    return render_template('header.html', current_user=flask_login.current_user) + \
-           render_template('object.html', form1=form1, form2=form2, ssoform=ssoform, object=None,
-                           message=message, urls = urls) + \
-           render_template('footer.html')
+    return (render_template('header.html') +#, current_user=flask_login.current_user) +
+            render_template('object.html', form1=form1, form2=form2, ssoform=ssoform, object=None,
+                            message=message, urls = urls) +
+            render_template('footer.html'))
 
 
 # TODO: make show_objects prettier, add parts of it to 'objects' after search completes
@@ -249,15 +255,15 @@ def show_objects(ident):
         # TODO: redirect or show "there is no object %s" % (ident,)
         pass
     elif not flask_login.current_user:
-        return render_template('header.html',  current_user=flask_login.current_user) + \
-               render_template('show_object.html')
+        return (render_template('header.html',  current_user=flask_login.current_user) +
+                render_template('show_object.html'))
     else:
         query = ("SELECT atomicrequest.id, atomicrequest.exptime, atomicrequest.status, request.program_id FROM request"
                  " JOIN object ON object.id = request.object_id "
                  "JOIN atomicrequest ON request.id = atomicrequest.request_id"
                  "WHERE object.id = '%s';" % (info[5],))
         obs = db.execute_sql(query)
-        ???
+        # TODO: remove above query?
         # TODO: setup sso object info (below)
         """
         if info[3] == 'e':
@@ -280,15 +286,57 @@ def show_objects(ident):
 
 
 
-    return render_template('header.html', current_user=flask_login.current_user) + \
-           render_template('show_object.html', object=ident) + \
-           render_template('footer.html')
+    return (render_template('header.html') +#, current_user=flask_login.current_user) +
+            render_template('show_object.html', object=ident) +
+            render_template('footer.html'))
 
 
 @app.route('/project_stats/<path:project>')
 def project_stats(project):
     # TODO: show chart with 'total time allocated', 'time requested', "time observed"
-    return render_template('header.html', current_user=flask_login.current_user) + render_template('') + render_template('footer.html')
+
+    day = datetime.datetime.now()
+    start = Time(day - datetime.timedelta(days=day.weekday())).mjd
+    end = Time(start + datetime.timedelta(days=7)).mjd
+
+    areq_query = ("SELECT atomicrequest.exptime, atomicrequest.status FROM atomicrequest"
+                  "JOIN request ON request.id = atomicrequest.request_id"
+                  "WHERE request.project_id = '%s'"
+                  #" AND  atomicrequest.lastmodified < %s AND atomicrequest.lastmodified > %s"
+                  ";" % (group_dict[project],))
+    requests = np.array(db.execute_sql(areq_query))
+    # TODO: test for whether there are any matching requests
+    pending = (requests.T[0] == 'PENDING')
+    pending_time = sum(requests.T[1][pending])/3600.
+    observed = ((requests.T[0] == 'OBSERVED') | (requests.T[0] == 'REDUCED'))
+    observed_time = sum(requests.T[1][observed])/3600.
+    # TODO: make time_allocated part of group definition?
+    time_allocated = 14.
+    # TODO: use better method than below (pygal or flask-appbuilder?)
+    import StringIO
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+
+    fig = Figure()
+    ax = fig.add_subplot(111)
+    labels = ['observed', 'pending', 'over_budget', 'free']
+    if observed_time+pending_time > time_allocated:
+        total_time = observed_time+pending_time
+        fracs = [observed_time/total_time, (time_allocated-observed_time)/total_time, (total_time-time_allocated)/total_time, 0.]
+    else:
+        fracs = [observed_time/time_allocated, pending_time/time_allocated, 0., (time_allocated-pending_time-observed_time)]
+    ax.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True)
+    canvas = FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response = make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+
+
+# return response
+    return (render_template('header.html', current_user=flask_login.current_user) +
+            render_template('project_stats', img_data=response) +
+            render_template('footer.html'))
 
 
 if __name__ == '__main__':
