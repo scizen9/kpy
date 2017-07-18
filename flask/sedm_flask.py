5d1cb62455e6fb0176336fa1e92bfb2bc640f8ba
kvyh <karlvyh@hotmail.com>
2017-07-18 10:42:48 -0700

    adding object_stats

--- flask/sedm_flask.py
+++ flask/sedm_flask.py
@@ -12,7 +12,7 @@ import numpy as np
 import pygal
 from pygal.style import Style
 import json
-from flask import Flask, request, flash, redirect, render_template, url_for, make_response
+from flask import Flask, request, flash, redirect, render_template, url_for, make_response, get_flashed_messages
 from SEDMDb.SedmDb import SedmDB
 from SEDMDb.SedmDb_tools import DbTools
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -21,7 +21,10 @@ from forms import RequestForm, RedirectForm, FindObjectForm, LoginForm, SubmitOb
 import flask
 import flask_login
 from wtforms import Form, HiddenField
-
+from astropy.io import fits
+from plotly.offline import plot
+from plotly.graphobjs import Heatmap
+import matplotlib.pyplot as plt
 
 # config
 SECRET_KEY = 'secret'
@@ -236,7 +239,6 @@ def objects():
             render_template('footer.html'))
 
 
-# TODO: make show_objects prettier, add parts of it to 'objects' after search completes
 @app.route('/objects/<path:ident>')
 def show_objects(ident):
     # take name or id and show info about it and images with permission
@@ -244,52 +246,107 @@ def show_objects(ident):
         iden = int(ident)
     except ValueError:
         name = str(ident)
-        iden = None
-    if iden:
-        info = db.get_from_object(['name', 'ra', 'dec', 'typedesig', 'epoch', 'id'], {'id': iden})[0]
-    elif name:
-        info = db.get_from_object(['name', 'ra', 'dec', 'typedesig', 'epoch', 'id'], {'name': name})[0]
+        iden = db.get_from_object(['id'], {'name': name})
+        if not iden:
+            flash("Object with name '%s' not found, please search for object" % (ident,))
+            redirect(url_for('objects'))
+        elif iden[0] == -1:
+            flash("Searching for name '%s' caused error '%', please report issue" % (ident, iden[1]))
+            redirect(url_for('objects'))
+        else:
+            redirect(url_for('show_objects', iden[0][0]))
 
-    print info
+    info = db.get_from_object(['name', 'ra', 'dec', 'typedesig', 'epoch', 'id'], {'id': iden})[0]
+    # TODO: work in SSO objects
     if info[0] == -1:
-        # TODO: show a query failed with message ""
-        # send report?
-        pass
+        flash("Searching for id '%s' caused error '%s', please report issue")
+        redirect(url_for('objects'))
     elif not info:
-        # TODO: redirect or show "there is no object %s" % (ident,)
-        pass
-    elif not flask_login.current_user:
-        return (render_template('header.html',  current_user=flask_login.current_user) +
-                render_template('show_object.html'))
-    else:
-        query = ("SELECT atomicrequest.id, atomicrequest.exptime, atomicrequest.status, request.program_id FROM request "
-                 "JOIN object ON object.id = request.object_id "
-                 "JOIN atomicrequest ON request.id = atomicrequest.request_id "
-                 "WHERE object.id = '%s';" % (info[5],))
-        obs = db.execute_sql(query)
-        # TODO: remove above query?
-        # TODO: setup sso object info (below)
-        """
-        if info[3] == 'e':
-            sso_query =
-        elif info[3] == 'h':
-
-        elif info[3] == 'p':
-
-        elif info[3] == 'E':
-        """
-        for areq in obs:
-            pass
-        # get the time, duration and file for observations of the object
-        areq_query = ("SELECT observation.mjd, observation.exptime, observation.fitsfile FROM observation"
-                      "JOIN object ON object.id = observation.object_id"
-                      "JOIN request ON request.id = observation.request_id"
-                      "WHERE object.id = '%s' AND request.program_id IN %s;" % (info[5], flask_login.current_user.groups))
-        observations = db.execute_sql(areq_query)
+        flash("Searching for id '%s' produced no results, please search for object")
+        redirect(url_for('objects'))
 
 
-    return (render_template('header.html') +#, current_user=flask_login.current_user) +
-            render_template('show_object.html', object=ident) +
+    # retrieve all of the requests submitted for the object
+    request_query = ("SELECT u.name, u.username, r.program_id, r.inidate, r.enddate, r.priority, r.status "
+                     "FROM request r, users u, object o WHERE r.user_id = u.id AND r.object_id = o.id "
+                     "AND r.object_id = '%s';" % (ident,))
+                    # TODO: add ```AND r.program_id IN %s;" % (,flask_login.current_user.groups)```
+    req = db.execute_sql(request_query)
+    for n, x in enumerate(req):
+        req[n] = list(x)
+        # decide between name/username
+        if not [0]:
+            del req[n][0]
+        else:
+            del req[n][1]
+        # decode from program_id to program name
+        req[n][1] = group_dict[req[n][1]]
+
+    # get the time, duration and file for observations of the object
+    a_query = ("SELECT obs.mjd, obs.exptime, a.filter, obs.fitsfile "
+               "FROM observation obs, object obj, atomicrequest a, request r WHERE o.id = obs.object_id AND "
+               "a.id = obs.atomicrequest_id AND r.id = obs.request_id "
+               "AND WHERE object.id = '%s';" % (ident,))
+               # TODO: add ```AND r.program_id IN %s;" % (, flask_login.current_user.groups)```
+    observations = db.execute_sql(a_query)
+    obs = []
+
+    def make_image(fitsfile):
+        data = fits.open(fitsfile)[0].data
+        mydiv = plot([Heatmap(z=data, output_type='div')])
+        return mydiv
+
+    for ob in observations:
+        obs.append(([ob[0], ob[1], ob[2]], make_image(ob[3])))
+
+    # TODO: make sure the above works
+    # TODO: check again
+    # TODO: check even more
+    """
+       day = datetime.datetime.now()
+       start = Time(day - datetime.timedelta(days=day.weekday())).mjd
+       end = start + 7
+       # TODO: make a form to set start/end dates
+       areq_query = ("SELECT atomicrequest.exptime, atomicrequest.status FROM atomicrequest "
+                     "JOIN request ON request.id = atomicrequest.request_id "
+                     "WHERE request.program_id = '%s'"
+                     # " AND  atomicrequest.lastmodified < %s AND atomicrequest.lastmodified > %s"
+                     ";" % (group_dict[project],))
+       requests = np.array(db.execute_sql(areq_query))
+       # TODO: test for whether there are any matching requests
+       pending = (requests.T[0] == 'PENDING')
+       pending_time = sum(requests.T[1][pending]) / 3600.
+       observed = ((requests.T[0] == 'OBSERVED') | (requests.T[0] == 'REDUCED'))
+       observed_time = sum(requests.T[1][observed]) / 3600.
+       # TODO: make time_allocated part of group definition?
+       time_allocated = 14.
+       # pygal graphing
+
+       # use different color schemes based on whether there is more time requested than allowed
+       if observed_time + pending_time > time_allocated:
+           c_style = Style(
+               background='transparent',
+               colors=('#52e852', '#1a4fba', '#e22626')
+           )
+           pi_chart = pygal.Pie(style=c_style, height=300, width=300)
+           pi_chart.title = "Time allocation (h)"
+           pi_chart.add('observed', observed_time)
+           pi_chart.add('requested', (time_allocated - observed_time))
+           pi_chart.add('requested beyond allocation', (observed_time + pending_time - time_allocated))
+       else:
+           c_style = Style(
+               background='transparent',
+               colors=('#52e852', '#1a4fba', '#e5f2de')
+           )
+           pi_chart = pygal.Pie(style=c_style, height=300, width=300)
+           pi_chart.title = "Time allocation (h)"
+           pi_chart.add('observed', observed_time)
+           pi_chart.add('requested', pending_time)
+           pi_chart.add('free', (time_allocated - pending_time - observed_time))
+       """
+
+    return (render_template('header.html') +  # , current_user=flask_login.current_user) +
+            render_template('object_stats.html', info=info, observations=obs, req_data=req) +
             render_template('footer.html'))
 
 
@@ -339,7 +396,7 @@ def project_stats(project):
     # retrieve all of the requests submitted for the project
     request_query = ("SELECT u.name, u.username, o.name, o.id, r.inidate, r.enddate, r.priority, r.status "
                      "FROM request r, users u, object o WHERE r.user_id = u.id AND r.object_id = o.id "
-                     "AND r.program_id = '%s';" % (group_dict[project]))
+                     "AND r.program_id = '%s';" % (group_dict[project],))
     req = db.execute_sql(request_query)
     for n, x in enumerate(req):
         req[n] = list(x)
