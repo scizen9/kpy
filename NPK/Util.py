3d7c4f21eca728266ad09419320ca111aa000741
Don Neill <neilljd@gmail.com>
2017-07-19 14:49:19 -0700

    Code cleanup

--- NPK/Util.py
+++ NPK/Util.py
@@ -1,27 +1,28 @@
 import numpy as np
-from astropy.coordinates import Angle 
+from astropy.coordinates import Angle
 
-'''
+"""
 Struct:
 http://stackoverflow.com/questions/1305532/convert-python-dict-to-object
-'''
+"""
+
 
 class Struct(object):
-    '''Converts a dictionary into an object
+    """Converts a dictionary into an object
 
     Example:
         a = {'a': 1, 'b': 2}
         o = Struct(a)
         print o.a, o.b
-    
-    '''
-    def __init__(self, **entries): 
-        "s = Struct(***dict)"
+
+    """
+    def __init__(self, **entries):
+        """s = Struct(***dict)"""
         self.__dict__.update(entries)
 
 
 def floatcompress(data, ndig=14):
-    '''Adapted from Finkbeiner IDL routine floatcompress'''
+    """Adapted from Finkbeiner IDL routine floatcompress"""
 
     t = data.dtype
     if not ((t == 'float32') or (t == 'float64')):
@@ -38,43 +39,47 @@ def floatcompress(data, ndig=14):
     return out
 
 
-
-def par_angle(HA, Dec, Lat=Angle("33d 21m 21.6s")):
-    ''' Compute parallactic angle given an hour angle and declination.
+def par_angle(ha, dec, lat=Angle("33d 21m 21.6s")):
+    """ Compute parallactic angle given an hour angle and declination.
 
     Parallactic angle is defined such that 180 is N/S.
 
-    Latitude defaults to Palomar. Must use Astropy Angle class'''
+    Latitude defaults to Palomar. Must use Astropy Angle class"""
 
-    if type(HA) != Angle: HA = Angle(HA, unit='hour')
-    if type(Dec) != Angle: Dec = Angle(Dec, unit='deg')
+    if type(ha) != Angle:
+        ha = Angle(ha, unit='hour')
+    if type(dec) != Angle:
+        dec = Angle(dec, unit='deg')
 
-    return Angle(np.arctan2(-np.sin(HA), 
-        np.cos(Dec)*np.tan(Lat) - np.sin(Dec)*np.cos(HA)))
+    return Angle(np.arctan2(-np.sin(ha),
+                            np.cos(dec) * np.tan(lat) - np.sin(dec) *
+                            np.cos(ha)))
 
 
-def air_index(lam, P=600, T=7, f=8):
-    ''' Returns index of refraction of air-1 at
+def air_index(lam, p=600, t=7, f=8):
+    """ Returns index of refraction of air-1 at
         lam in micron at vacuum
-        Pressure in mm hg
-        T is temperature in deg C
+        p is pressure in mm Hg
+        t is temperature in deg C
         f is water vapor pressure in mm Hg
-    '''
+    """
 
-    K1 = (1/lam)**2
-    nm1e6 = 64.328 + 29498.1/(146-K1) + 255.4/(41-K1)
+    k1 = (1/lam)**2
+    nm1e6 = 64.328 + 29498.1/(146-k1) + 255.4/(41-k1)
 
-    nm1e6 *= P * (1 + (1.049-0.0157 * T)*1e-6*P) / (720.883 * (1 + 
-        0.003661 * T))
+    nm1e6 *= p * (1 + (1.049-0.0157 * t)*1e-6*p) / (720.883 * (1 +
+                                                               0.003661 * t))
 
-    nm1e6 -= 0.0624 - 0.000680 * K1 / (1 + 0.003661 * T) * f
+    nm1e6 -= 0.0624 - 0.000680 * k1 / (1 + 0.003661 * t) * f
 
     return nm1e6/1e6
 
+
 def atm_disper(l2, l1, airmass, **kwargs):
-    ''' atmospheric dispersion in arcsecond between l2 and l1 in micron
+    """ atmospheric dispersion in arcsecond between l2 and l1 in micron
         at a given airmass. See air index for documentation on pressure,
-        temperature, and water vapor pressure'''
+        temperature, and water vapor pressure"""
 
     z = np.arccos(1.0/airmass)
-    return 206265 * (air_index(l2, **kwargs) - air_index(l1, **kwargs)) * np.tan(z)
+    return 206265 * (air_index(l2, **kwargs) - air_index(l1,
+                                                         **kwargs)) * np.tan(z)
