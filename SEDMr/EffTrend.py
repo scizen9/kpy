3452473276e0e19ad1041cdfb14c1011ab93f9c4
Don Neill <neilljd@gmail.com>
2017-09-29 15:50:19 -0700

    include jd in text file output for EffTrend.py

--- SEDMr/EffTrend.py
+++ SEDMr/EffTrend.py
@@ -88,6 +88,6 @@ ofil = os.path.join(sdir, 'SEDM_eff_trend.pdf')
 pl.savefig(ofil)
 with open('SEDM_eff_trend.txt', 'wb') as dfil:
     dat_writer = csv.writer(dfil, delimiter=" ", quoting=csv.QUOTE_MINIMAL)
-    dat_writer.writerows(izip(efs, ef1, ef2, ef3, ef4, ef5))
+    dat_writer.writerows(izip(jd, ef1, ef2, ef3, ef4, ef5, efs))
 
 
