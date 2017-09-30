bec13cd7cc93f9100d2d4b5f9095e668fd72e79b
Don Neill <neilljd@gmail.com>
2017-09-30 08:32:33 -0700

    typo fix

--- SEDMr/DrpReport.py
+++ SEDMr/DrpReport.py
@@ -107,7 +107,7 @@ def report():
         else:
             objname = "_".join(objname.split('_')[1:])
 
-        print("%-25s %4s %6s   %6.1f %4d %5s  %5.3f   %9s  %9-s  %s  %6s" %
+        print("%-25s %4s %6s   %6.1f %4d %5s  %5.3f   %9s  %-9s  %s  %6s" %
               (objname, obs, meth, expt, qual, ("on" if skysub else "off"),
                air, reducer, ctype, zmed, rlap))
     print("\nTotal quality (1-3) science exposure time = %.1f s" % totexpt)
