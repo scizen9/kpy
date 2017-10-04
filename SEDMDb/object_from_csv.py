53111671578b6015bc7606c31c5bfb5b76a81f76
Don Neill <neilljd@gmail.com>
2017-10-04 13:59:19 -0700

    adding explicit sql option for object creation

--- SEDMDb/object_from_csv.py
+++ SEDMDb/object_from_csv.py
@@ -8,19 +8,35 @@ db = SedmDb.SedmDB(dbname='sedmdbtest', host='localhost')
 
 
 def objects_from_csv(filename):
+    """
+    Opens a csv file with format name, ra, dec, mag(optional) and inputs it into the database
+    NOTE: assumes fixed object
+    """
     with open(filename, 'r') as csvfile:
-        reader = csv.reader(csvfile, )
+        reader = csv.reader(csvfile)
         next(reader, None)
         for row in reader:  # For each row, create the dictionary and submit
             pardic = {'name': row[0], 'ra': ra_to_decimal(row[1]), 'dec': dec_to_decimal(row[2])}
             pardic['typedesig'] = 'f'
-            mag = row[3] #not currently stored in object table
+            if len(row) > 3:
+                mag = row[3] #not currently stored in object table
             obj = db.add_object(pardic)
             print(obj)
+
+
+def objects_from_csv_allow_dup(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, )
+        next(reader, None)
+        for row in reader:  # For each row, create the dictionary and submit
+            db.execute_sql("INSERT INTO object (name, ra, dec, typedesig) VALUES ('%s', %s, %s, '%s')"
+                           % (row[0], ra_to_decimal(row[1]), dec_to_decimal(row[2]), 'f'))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', type=str)
     args = parser.parse_args()
-    f = args.file
-    objects_from_csv(f)
+    if args.file:
+        f = args.file
+        objects_from_csv(f)
