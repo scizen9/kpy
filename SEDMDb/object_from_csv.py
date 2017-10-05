abeb8eeb61068486dab12338fa1297b1090e61c6
Nadejda Blagorodnova <nadejda14@gmail.com>
2017-10-05 16:32:46 -0700

    Slightly modified script, which allows to start ingesting from a certain number.

--- SEDMDb/object_from_csv.py
+++ SEDMDb/object_from_csv.py
@@ -4,7 +4,7 @@ import SedmDb
 from SedmDb_tools import dec_to_decimal, ra_to_decimal
 import argparse
 
-db = SedmDb.SedmDB(dbname='sedmdbtest', host='localhost')
+db = SedmDb.SedmDB(dbname='sedmdb', host='localhost')
 
 
 def objects_from_csv(filename):
@@ -24,13 +24,14 @@ def objects_from_csv(filename):
             print(obj)
 
 
-def objects_from_csv_allow_dup(filename):
+def objects_from_csv_allow_dup(filename, lastid=250910):
     with open(filename, 'r') as csvfile:
         reader = csv.reader(csvfile, )
         next(reader, None)
         for row in reader:  # For each row, create the dictionary and submit
-            db.execute_sql("INSERT INTO object (name, ra, dec, typedesig) VALUES ('%s', %s, %s, '%s')"
-                           % (row[0], ra_to_decimal(row[1]), dec_to_decimal(row[2]), 'f'))
+            db.execute_sql("INSERT INTO object (id, name, ra, dec, typedesig, magnitude) VALUES (%d, '%s', %s, %s, '%s', mag)"
+                           % (lastid, row[0], ra_to_decimal(row[1]), dec_to_decimal(row[2]), 'f', row[3]))
+            lastid = lastid + 1
 
 
 if __name__ == '__main__':
@@ -39,4 +40,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.file:
         f = args.file
-        objects_from_csv(f)
+        objects_from_csv_allow_dup(f)
