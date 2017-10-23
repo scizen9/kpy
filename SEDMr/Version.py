1f3f3a71668774ef85f34d31637b4bd1a3e965c3
Don Neill <neilljd@gmail.com>
2017-10-22 19:51:03 -0700

    attempt to make Version.py more robust

--- SEDMr/Version.py
+++ SEDMr/Version.py
@@ -1,11 +1,17 @@
 import subprocess
 import os
+import datetime
 
 
 def ifu_drp_version():
-    cwd = os.getcwd()
-    home = os.environ["HOME"]
-    os.chdir(home+"/kpy/SEDMr")
-    ver = subprocess.check_output(["git", "describe", "--always"]).strip()
-    os.chdir(cwd)
+    try:
+        cwd = os.getcwd()
+        home = os.environ["HOME"]
+        os.chdir(home+"/kpy/SEDMr")
+        ver = subprocess.check_output(["git", "describe", "--always"],
+                                      shell=True).strip()
+        os.chdir(cwd)
+    except:
+        ver = "%s" % datetime.datetime.now()
+
     return ver
