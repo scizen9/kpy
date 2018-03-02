60e63b1f42753603307912269110ad568c4a4b62
Don Neill <neilljd@gmail.com>
2018-03-02 08:19:09 -0800

    more robust checking of REQ_ID

--- SEDMr/Check.py
+++ SEDMr/Check.py
@@ -190,6 +190,8 @@ def check_spec(specname, corrname='std-correction.npy', redshift=0, smoothing=0,
             dec = ''
         if 'REQ_ID' in hdr:
             req_id = hdr['REQ_ID']
+            if len(req_id) == 0:
+                req_id = 0
         else:
             req_id = 0
     else:
